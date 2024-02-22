c06771d18086f2c7686d53939270eb6c94e56cf4
maranan.312474 <maranan.312474@LAB-A-PC28>
2024-02-23 07:47:07 +0800

    Add project files.

---
+++ ComputeAverageApp/ComputeAverageProgram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ComputeAverageApp
+{
+    internal class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 5 grades separated by new line: ");
+            double firstgrade = Convert.ToDouble(Console.ReadLine());
+            double secondgrade = Convert.ToDouble(Console.ReadLine());
+            double thirdgrade = Convert.ToDouble(Console.ReadLine());
+            double fourthgrade = Convert.ToDouble(Console.ReadLine());
+            double fifthgrade = Convert.ToDouble(Console.ReadLine());
+            double sum = firstgrade + secondgrade + thirdgrade + fourthgrade + fifthgrade;
+            double totalgrade = sum / 5;
+            double average = Math.Round(totalgrade);
+            Console.WriteLine("The average is " + totalgrade + " and round off to " + average);
+            
+            
+        }
+    }
+}
